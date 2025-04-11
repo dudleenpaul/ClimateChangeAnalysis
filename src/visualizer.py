e8a296431b8f89aa44c75acb771c374e83dc231f
TheBrunster <70962135+TheBrunster@users.noreply.github.com>
2025-04-11 17:34:05 -0400

    Add docstrings in visualizer.py

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,7 +1,21 @@
+""" visualizer.py
+Contains methods for visualizing temperature data
+
+Methods:
+    plot_predictions(y_true, y_pred): plot actual vs predicted temperature values
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_predictions(y_true: np.ndarray, y_pred: np.ndarray):
+    """
+	Plot actual vs predicted temperature values.
+
+	Args: 
+        y_true (ndarray): actual temperature values
+        y_pred (ndarray): predicted temperature values
+    """
     plt.figure(figsize=(8, 6))
     plt.scatter(range(len(y_true)), y_true, label='Actual', alpha=0.7)
     plt.plot(range(len(y_pred)), y_pred, label='Predicted', color='red')
