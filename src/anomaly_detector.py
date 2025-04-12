1536c8ef441ab5a20c38251981b85912ae9db573
TheBrunster <70962135+TheBrunster@users.noreply.github.com>
2025-04-11 22:28:28 -0400

    Add docstrings in anomaly_detector.py

--- src/anomaly_detector.py
+++ src/anomaly_detector.py
@@ -3,6 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def detect_temperature_anomalies(csv_path: str, z_thresh: float = 2.0):
+    """
+    Detect and plot temperature anomalies directly from the CSV file
+
+    Args:
+        csv_path (str): the filepath of the CSV file containing temperature data
+    Kwargs:
+        z_thresh (float): the threshold for a temperature to be considered an anomaly
+    Returns:
+        a list of anomalies
+    """
     df = pd.read_csv(csv_path)
     mean_temp = df['Mean'].mean()
     std_temp = df['Mean'].std()
