0636c7dc4a377cc09375b88363de1dbecfc5d954
TheBrunster <70962135+TheBrunster@users.noreply.github.com>
2025-04-11 22:41:53 -0400

    Add docstrings in climate_clustering.py

--- src/climate_clustering.py
+++ src/climate_clustering.py
@@ -4,6 +4,16 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 def cluster_temperature_patterns(csv_path: str, n_clusters: int = 3):
+    """
+    Perform clustering anaylsis on climate data using K-means algorithm and visualize results.
+
+    Args:
+        csv_path (str): string representation of filepath containing the CSV temperature data
+    Kwargs:
+        n_clusters (int): number of clusters/groups
+    Returns:
+        Modified DataFrame with the additonal 'Cluster' column to show which cluster it belongs to
+    """
     df = pd.read_csv(csv_path)
     df = df.dropna()
     X = df[['Year', 'Mean']].values  # NOAA format
